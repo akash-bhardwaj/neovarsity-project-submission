a41921580db7e211463278bcc6078c067772b886
Akash Bhardwaj <akashb@appdynamics.com>
2025-01-12 13:35:44 +0530

    Adding Basic Spring Boot Project Structure

---
+++ src/main/java/com/scaler/neovarsityproject/NeovarsityprojectApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.neovarsityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeovarsityprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeovarsityprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/neovarsityproject/NeovarsityprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.neovarsityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeovarsityprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
